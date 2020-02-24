9a66f8344e7934ff496cea2177f31c2110208185
Jeff <rustyangel@gmail.com>
2020-02-24 15:08:17 -0700

    Chore: Update package configs

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        node: true,
+        es6: true,
+        'jest/globals': true,
+    },
+    extends: ['standard'],
+    plugins: ['jest', '@typescript-eslint'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 9,
+        sourceType: 'module',
+        // project: './tsconfig.json',
+    },
+    rules: {
+        'arrow-parens': ['error', 'always'],
+
+        'comma-dangle': ['error', 'always-multiline'],
+
+        // Allow async-await
+        'generator-star-spacing': 'off',
+
+        indent: ['error', 4],
+    },
+}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+
+import { wait } from './wait'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    try {
+        const ms: string = core.getInput('milliseconds')
+        core.debug(`Waiting ${ms} milliseconds ...`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+        core.debug(new Date().toTimeString())
+        await wait(parseInt(ms, 10))
+        core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+        core.setOutput('time', new Date().toTimeString())
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
 
 run()
