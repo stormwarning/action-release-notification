5cc9e788fc4ef604a12881caadf58d63a261ce69
Jeff <rustyangel@gmail.com>
2020-03-03 22:20:17 -0700

    Update: Use Slack SDK instead of raw http request

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import * as core from '@actions/core'
+import { ChatPostMessageArguments, WebClient } from '@slack/web-api'
 
-import { buildMessage, optionalParams, sendMessage } from './slack'
+import { buildActionButton } from './slack'
+
+interface ButtonOptions {
+    text: string
+    url?: string
+    value?: string
+}
 
 const stringify = (data: any) => JSON.stringify(data, undefined, 2)
 
@@ -9,14 +16,34 @@ const getRequired = (name: string): string =>
 
 export async function run (): Promise<void> {
     try {
-        let token = getRequired('slack-bot-user-oauth-access-token')
-        let channel = getRequired('slack-channel')
-        let text = getRequired('slack-text')
+        let channel = getRequired('channel')
+        let message = getRequired('message')
+        let actions: ButtonOptions[] = JSON.parse(core.getInput('actions'))
+        let token = process.env.SLACKBOT_TOKEN
+        let slack = new WebClient(token)
 
-        let message = buildMessage(channel, text, optionalParams())
-        let result = await sendMessage(token, message)
+        if (!channel) {
+            core.setFailed('You must provide a `channel`')
+            return
+        }
 
-        let resultAsJson = stringify(result)
+        let elements = actions.map(({ text, url }) =>
+            buildActionButton({ text, url }),
+        )
+        let args: ChatPostMessageArguments = {
+            channel,
+            text: message,
+            blocks: [
+                {
+                    type: 'actions',
+                    elements,
+                },
+            ],
+            mrkdwn: true,
+        }
+
+        let response = await slack.chat.postMessage(args)
+        let resultAsJson = stringify(response)
 
         core.setOutput('slack-result', resultAsJson)
     } catch (error) {
--- src/slack.ts
+++ src/slack.ts
@@ -1,96 +1,22 @@
-import * as https from 'https'
+import { Button } from '@slack/types'
 
-interface Option {
-    [key: string]: string | undefined
+interface ButtonOptions {
+    text: string
+    url?: string
+    value?: string
 }
 
-interface Message {
-    [key: string]: string | undefined
-}
+export function buildActionButton (options: ButtonOptions): Button {
+    let { text, url, value } = options
 
-interface Response {
-    statusCode: number | undefined
-    result: string
-}
-
-export function optionalParams () {
-    let opt: Option = {}
-    let { env } = process
-
-    Object.keys(env)
-        .filter((key) => key.toUpperCase().startsWith('INPUT_SLACK-OPTIONAL-'))
-        .forEach((key) => {
-            let slackKey = key
-                .replace('INPUT_SLACK-OPTIONAL-', '')
-                .toLowerCase()
-            opt[slackKey] = env[key]
-        })
-
-    return opt
-}
-
-export const buildMessage = (channel: string, text: string, optionals: Option = {}) => {
-    let message: Message = {
-        channel,
-        text,
-    }
-
-    Object.keys(optionals).forEach((name) => {
-        message[name] = optionals[name]
-    })
-
-    return message
-}
-
-const getOptions = (token: string) => {
     return {
-        hostname: 'slack.com',
-        port: 443,
-        path: '/api/chat.postMessage',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            Authorization: `Bearer ${token}`,
+        type: 'button',
+        text: {
+            type: 'plain_text',
+            text,
+            emoji: true,
         },
+        url,
+        value,
     }
-}
-
-const post = (token: string, message: Message): Promise<Response> => {
-    return new Promise((resolve, reject) => {
-        let payload = JSON.stringify(message)
-        let options = getOptions(token)
-
-        let req = https.request(options, (res) => {
-            let chunks: any[] = []
-
-            res.on('data', (chunk) => {
-                chunks.push(chunk)
-            })
-
-            res.on('end', () => {
-                resolve({
-                    statusCode: res.statusCode,
-                    result: Buffer.concat(chunks).toString(),
-                })
-            })
-        })
-
-        req.on('error', (error) => {
-            reject(error)
-        })
-
-        req.write(payload)
-        req.end()
-    })
-}
-
-export const sendMessage = async (token: string, message: Message) => {
-    let response = await post(token, message)
-    let result = JSON.parse(response.result)
-
-    if (!result || !result.ok || response.statusCode !== 200) {
-        throw new Error(`Error! ${JSON.stringify(response)}`)
-    }
-
-    return response
 }
