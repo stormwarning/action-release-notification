acbb989b739b3990b49f810663b04d5930d681bb
Jeff <rustyangel@gmail.com>
2020-03-03 22:20:26 -0700

    Chore: Add tests

---
+++ __tests__/slack.test.ts
@@ -0,0 +1,11 @@
+import { buildActionButton } from '../src/slack'
+
+describe('build action button', () => {
+    it('builds a button object with a url', () => {
+        let button = buildActionButton({ text: 'Text', url: 'localhost' })
+
+        expect(button.text.text).toEqual('Text')
+        expect(button.url).toEqual('localhost')
+        expect(button.value).not.toBeDefined()
+    })
+})
